30e64b3db2c2ee5592bf2d3ee44aa87c8b6d5945
don-briski <abdullahiemoabino2@gmail.com>
2023-06-01 08:40:19 +0100

    hide the tech in mobile view

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -7,7 +7,7 @@ import { technologies } from '../constants'
 
 const Tech = () => {
   return (
-    <div className='flex flex-row flex-wrap justify-center
+    <div className='xs:hidden flex flex-row flex-wrap justify-center
     gap-10'>
       {technologies.map((technology, index) => (
         <div className='w-28 h-28' key={technology.name}>
